e2ef56d0df82560fe14560ae2652561eb90ba57a
umer khalique <omerkhalique13@gmail.com>
2022-03-01 20:33:45 +0500

    update

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ const btn3 = document.querySelector(".btn3");
 
 
 btn1.addEventListener("click",function(){
-    if(btn1.classList.contains("fa-plus")){
-        btn1.classList.remove('fa-plus');
-        btn1.classList.add('fa-minus');
-        question1.style.display = "none";
-    }else{
+    if(btn1.classList.contains("fa-minus")){
         btn1.classList.remove('fa-minus');
         btn1.classList.add('fa-plus');
+        question1.style.display = "none";
+    }else{
+        btn1.classList.remove('fa-plus');
+        btn1.classList.add('fa-minus');
         question1.style.display = "block";
     }
 });
 
 btn2.addEventListener("click",function(){
-    if(btn2.classList.contains("fa-plus")){
-        btn2.classList.remove('fa-plus');
-        btn2.classList.add('fa-minus');
-        question2.style.display = "none";
-    }else{
+    if(btn2.classList.contains("fa-minus")){
         btn2.classList.remove('fa-minus');
         btn2.classList.add('fa-plus');
+        question2.style.display = "none";
+    }else{
+        btn2.classList.remove('fa-plus');
+        btn2.classList.add('fa-minus');
         question2.style.display = "block";
     }
 });
 
 btn3.addEventListener("click",function(){
-    if(btn3.classList.contains("fa-plus")){
-        btn3.classList.remove('fa-plus');
-        btn3.classList.add('fa-minus');
-        question3.style.display = "none";
-    }else{
+    if(btn3.classList.contains("fa-minus")){
         btn3.classList.remove('fa-minus');
         btn3.classList.add('fa-plus');
+        question3.style.display = "none";
+    }else{
+        btn3.classList.remove('fa-plus');
+        btn3.classList.add('fa-minus');
         question3.style.display = "block";
     }
 });
